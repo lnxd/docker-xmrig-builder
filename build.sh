35e8f7030e10c16ade336a8ef9067eab9f988b93
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-27 21:29:54 +1100

    Added extra branch

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ docker rm xmrig-builder
 docker build . -t xmrig-builder
 docker run -e TZ="Australia/Melbourne" -v $PWD/build-output:/home/docker/build-output -e SOURCE="--depth 1 https://github.com/lnxd/xmrig.git" --name="xmrig-builder" xmrig-builder
 mv $PWD/build-output/xmrig.tar.gz $PWD/build-output/xmrig-fee.tar.gz
+
 docker rm xmrig-builder
 docker run -e TZ="Australia/Melbourne" -v $PWD/build-output:/home/docker/build-output -e SOURCE="--depth 1 --branch no-fee https://github.com/lnxd/xmrig.git" --name="xmrig-builder" xmrig-builder
 mv $PWD/build-output/xmrig.tar.gz $PWD/build-output/xmrig-no-fee.tar.gz
+
+docker rm xmrig-builder
+docker run -e TZ="Australia/Melbourne" -v $PWD/build-output:/home/docker/build-output -e SOURCE="--depth 1 --branch lnxd-fee https://github.com/lnxd/xmrig.git" --name="xmrig-builder" xmrig-builder
+mv $PWD/build-output/xmrig.tar.gz $PWD/build-output/xmrig-lnxd-fee.tar.gz
